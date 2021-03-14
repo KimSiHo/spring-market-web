d3522ac919387bf553e9f57e6ee4f6161fd7257d
Kim SiHo <kim125y@naver.com>
2021-03-14 19:35:51 +0900

    latest

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -9,7 +9,7 @@ cp $REPOSITORY/zip/target/*.jar $REPOSITORY/
 
 echo "> 현재 구동 중인 애플리케이션 pid 확인"
 
-CURRENT_PID=$(pgrep -f1 spring-market-web | grep jar | awk '{print $1}')
+CURRENT_PID=$(pgrep -fl spring-market-web | grep jar | awk '{print $1}')
 
 echo "> 현재 구동 중인 애플리케이션 pid: $CURRENT_PID"
 
@@ -17,7 +17,7 @@ if [ -z "$CURRENT_PID" ]; then
         echo "> 현재 구동 중인 애플리케이션이 없으므로 종료하지 않습니다."
 else
         echo "> kill -15 $CURRENT_PID"
-        kill -15 $CURRENT_PID
+        kill -15 $CURRENT_jPID
         sleep 5
 fi
 
