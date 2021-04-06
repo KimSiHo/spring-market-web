b325ccdc2a11b95e89c5c20de9e889273d5a0c61
Kim SiHo <kim125y@naver.com>
2021-04-06 09:21:40 +0900

    latest

--- src/main/java/com/jpabook/jpashop/common/file/service/localFileUploadService.java
+++ src/main/java/com/jpabook/jpashop/common/file/service/localFileUploadService.java
@@ -10,6 +10,7 @@ import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
+import javax.annotation.PostConstruct;
 import java.io.File;
 import java.io.IOException;
 import java.time.LocalDateTime;
