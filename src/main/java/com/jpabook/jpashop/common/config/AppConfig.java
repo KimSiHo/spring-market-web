d89f8b1296a92ca4805fa2442df00ff4c3d77764
Kim SiHo <kim125y@naver.com>
2021-04-09 05:49:47 +0900

    latest

--- src/main/java/com/jpabook/jpashop/common/config/AppConfig.java
+++ src/main/java/com/jpabook/jpashop/common/config/AppConfig.java
@@ -1,6 +1,5 @@
 package com.jpabook.jpashop.common.config;
 
-import javafx.beans.property.Property;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
