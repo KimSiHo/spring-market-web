adfa9bf17c24719cefde2f0c39c429493cbb3fcd
Kim SiHo <kim125y@naver.com>
2021-04-09 04:40:35 +0900

    latest

--- src/main/java/com/jpabook/jpashop/common/config/AppConfig.java
+++ src/main/java/com/jpabook/jpashop/common/config/AppConfig.java
@@ -4,12 +4,9 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class AppConfig {
-
     @Bean
     public PasswordEncoder passwordEncoder() {
         return PasswordEncoderFactories.createDelegatingPasswordEncoder();
